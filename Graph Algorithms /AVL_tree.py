c996b30f92d4a2b2b38a6ddc3290cc3778a93414
samriddha kc <samriddhakc@samriddhas-MacBook-Pro.local>
2019-01-20 16:51:25 -0600

    added_delete_BST

--- Graph Algorithms /AVL_tree.py
+++ Graph Algorithms /AVL_tree.py
@@ -21,39 +21,99 @@ class AVL_Tree:
         else:
             iter.left=self._insert(iter.left,key)
         #update the height of the ancestor nodes
-        print("the current key-iter val is",(key,iter.curr_val))
         iter.height=1+max(self.getHeight(iter.left),self.getHeight(iter.right))
         #get balance factor of the nodes
         balance=self.getBalance(iter)
         #step 4-If the node is unbalanced, then try out the four possible  cases for rotation
         #Case 1:Left-left
         if balance>1 and key<iter.left.curr_val:
-            print("Loop1")
             return self.rightRotate(iter)
 
         #Case 2:Right-Right
         if balance<-1 and key>iter.right.curr_val:
-            print("Loop2")
             return self.leftRotate(iter)
 
         #Case 3: Left-Right
         if balance>1 and key>iter.left.curr_val:
-            print("Loop3")
             iter.left=self.leftRotate(iter.left)
             return self.rightRotate(iter)
 
         #Case 4: Right-Left
         if balance<-1 and key<iter.right.curr_val:
-            print("Loop4")
             iter.right=self.rightRotate(iter.right)
             return self.leftRotate(iter)
 
         return iter
 
+    def _delete(self,iter,prev,key):
+        if iter.curr_val==key:
+            if prev==None:
+                if iter.right==None and iter.left==None:
+                    return None
+                elif iter.right!=None and iter.left==None:
+                    return iter.right
+                elif  iter.right==None and iter.left!=None:
+                    return iter.left
+                elif iter.right!=None and iter.left!=None:
+                    min_right_Tree=self._mini_val(iter,iter.right)
+                    min_right_Tree.right=iter.right
+                    min_right_Tree.left=iter.left
+                    #iter.right=None
+                    return min_right_Tree
+            else:
+                if prev.right==iter:
+                    if iter.right==None and iter.left==None:
+                        prev.right=None
+                        return prev
+                    elif iter.right!=None and iter.left==None:
+                        prev.right=iter.right
+                        return prev
+                    elif  iter.right==None and iter.left!=None:
+                        prev.right=iter.left
+                        return prev
+                    elif iter.right!=None and iter.left!=None:
+                        min_right_Tree=self._mini_val(iter,iter.right)
+                        prev.right=min_right_Tree
+                        min_right_Tree.right=iter.right
+                        min_right_Tree.left=iter.left
+                        return prev
+
+                else:
+                    if iter.right==None and iter.left==None:
+                        prev.left=None
+                        return prev
+                    elif iter.right!=None and iter.left==None:
+                        prev.left=iter.right
+                        return prev
+                    elif  iter.right==None and iter.left!=None:
+                        prev.left=iter.left
+                        return prev
+                    elif iter.right!=None and iter.left!=None:
+                        min_right_Tree=self._mini_val(iter,iter.right)
+                        prev.left=min_right_Tree
+                        min_right_Tree.left=iter.left
+                        return prev
+
+        else:
+            if iter.left!=None:
+                self._delete(iter.left,iter,key)
+            if iter.right!=None:
+                self._delete(iter.right,iter,key)
+        return iter
+
+
     def getBalance(self,var_node):
         if not var_node:
             return 0
         return self.getHeight(var_node.left)-self.getHeight(var_node.right)
+
+    def _mini_val(self,prev,cur_node):
+            if cur_node.left==None: #case where the deleted node does not have any child
+                prev.left=cur_node.right
+                return cur_node
+            else:
+                return self._mini_val(cur_node,cur_node.left)
+
 
     def getHeight(self,var_node):
         if not var_node:
@@ -121,6 +181,9 @@ def main():
     iter=AVL_t._insert(iter,30)
     iter=AVL_t._insert(iter,40)
     iter=AVL_t._insert(iter,25)
-    #AVL_t.print_pre_order(iter)
+    iter=AVL_t._delete(iter,None,20)
+    #print(AVL_t._mini_val(None,20))
+    #print(AVL_t._mini_val(None,30))
+    AVL_t.print_pre_order(iter)
 
 main()
